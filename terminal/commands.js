37ccd71175fb0537d901fb4794ececcda33591ed
Pacharapon Naewraj <martdafuq@gmail.com>
2023-03-24 08:07:32 +0700

    create new js file

---
+++ terminal/commands.js
@@ -0,0 +1,75 @@
+var password = "nottoday";
+var linkedin = "https://www.linkedin.com/in/pacharaponnaewraj/";
+var github = "https://github.com/martdafuq/";
+var email = 'mailto:pacharapon.business@gmail.com';
+var weather = "https://martdafuq.github.io/weather/";
+var snake = "https://martdafuq.github.io/snake-game/";
+var coin = "https://martdafuq.github.io/flip-coin/"
+
+whois = [
+  "<br>",
+  "Hey, I'm Smart!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/pacharaponnaewraj' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/martdafuq' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+mini = [
+  "<br>",
+  'weather app    <a href="' + weather + '" target="_blank">martdafuq.github.io/weather/' + "</a>",
+  'snake game     <a href="' + snake + '" target="_blank">martdafuq.github.io/snake-game/' + "</a>",
+  'flip a coin    <a href="' + coin + '" target="_blank">martdafuq.github.io/flip-coin/' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is martdafuq?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">mini</span>           View my learning app',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">martdafuq Not A Corporation. Just A Normal Guy.</span>',
+  "                                __        __            ____                ",
+  "   ____ ___    ____ _   _____  / /_  ____/ /  ____ _   / __/  __  __  ____ _",
+  "  / __ `__ \\  / __ `/  / ___/ / __/ / __  /  / __ `/  / /_   / / / / / __ `/",
+  " :/ / / / / / / /_/ /  / /    / /_  / /_/ /  / /_/ /  / __/  / /_/ / / /_/ / ",
+  "/_/ /_/ /_/  \\__,_/  /_/     \\__/  \\__,_/   \\__,_/  /_/     \\__,_/  \\__, /  ",
+  "                                                                      /_/   ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
